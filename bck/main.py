389383085d5a31fad8214b5868103c013982f99e
LeylaAliyevaUlfet <eliyevaleyla127@gmail.com>
2026-09-20 00:02:08 +0400

    Initial commit

---
+++ bck/main.py
@@ -0,0 +1,18 @@
+import random
+import json
+choosen_word = []
+value=True
+with open("bck/words.json","r") as file:
+    data = json.load(file)
+i = random.randrange(1,3)
+choosen_word.append(data[i]["word"])
+choosen_word = str(choosen_word)
+
+print(choosen_word)
+add_word = list(input("Add word:  "))
+true_letters=[add_word[x].upper() if add_word[x] in choosen_word else add_word[x] for x in range(len(add_word)) ]
+
+
+print(true_letters)
+
+
