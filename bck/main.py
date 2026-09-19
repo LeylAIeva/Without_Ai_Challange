388fb57069671fc581c4d6b61bf098c6a13069e3
LeylaAliyevaUlfet <eliyevaleyla127@gmail.com>
2026-09-20 00:38:42 +0400

    point added but success doesnt work

--- bck/main.py
+++ bck/main.py
@@ -1,6 +1,7 @@
 import random
 import json
 choosen_word = []
+point = 4
 value=True
 with open("bck/words.json","r") as file:
     data = json.load(file)
@@ -12,7 +13,25 @@ print(choosen_word)
 add_word = list(input("Add word:  "))
 true_letters=[add_word[x].upper() if add_word[x] in choosen_word else add_word[x] for x in range(len(add_word)) ]
 
-
 print(true_letters)
+str_true_letters ="".join(true_letters).lower()
+while point>0:
+    
+        
+        if str_true_letters.lower() == choosen_word:
+              print("Congrats")
+              break
+        else:
+              add_word = list(input("Add word:  "))
+              point-=1
+              
+        print(point)
+        print(str_true_letters)
+        
+        
+              
+
+
+
 
 
