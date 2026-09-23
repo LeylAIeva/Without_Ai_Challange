0a42dfb38af86117d8ebda7e5d9c8f9861014f68
LeylaAliyevaUlfet <eliyevaleyla127@gmail.com>
2026-09-23 21:49:53 +0400

    error added

--- bck/main.py
+++ bck/main.py
@@ -12,32 +12,38 @@ choosen_word = str(choosen_word)
 print(choosen_word)
 
 
-
+count = len(choosen_word)
 while point>0:
+        
         add_word = list(input("Add word:  "))
         mod_word="".join(add_word).lower()
         true_letters=[add_word[x].upper() if add_word[x] in choosen_word else add_word[x] for x in range(len(add_word)) ]
         
-        str_true_letters="".join(true_letters).lower()
+        """str_true_letters="".join(true_letters).lower() unnecessary"""
         
-        print(f"str true letters{str_true_letters}")
-        if mod_word in choosen_word:
-              print("Congrats")
-              break
+        
+        
+        if len(mod_word) == count:
               
+            if mod_word in choosen_word:
+                  print("Congrats")
+                  break
+
+            else:
+
+
+                  point-=1
         else:
-              
-              
-              point-=1
-              
+              raise ValueError(f"it must be {count}")
               
         print(point)
         
         print(f"choosen_word {choosen_word}")
         
-        print(f"mod word {mod_word}")
+        print(f"mod word {type(mod_word)}")
         
         print(f"true letters {true_letters}")
+      
         
         
 
